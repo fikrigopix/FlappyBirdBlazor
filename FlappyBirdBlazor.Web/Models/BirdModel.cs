58c739e1fc0adb06cd7e99561fcce7dfb4a36f56
fikri <fikrigopix@gmail.com>
2021-02-23 15:16:32 +0700

    Add project files.

---
+++ FlappyBirdBlazor.Web/Models/BirdModel.cs
@@ -0,0 +1,26 @@
+﻿namespace FlappyBirdBlazor.Web.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; private set; } = 100;
+        public int JumpStrength { get; private set; } = 50;
+
+        public bool IsOnGround()
+        {
+            return DistanceFromGround <= 0;
+        }
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
+
+        public void Jump() 
+        {
+            if (DistanceFromGround <= 500)
+            {
+                DistanceFromGround += JumpStrength;
+            }
+        }  
+    }
+}
